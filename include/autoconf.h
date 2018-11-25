01832dbed9e5e5b1b657ad26de9934ba8543f2c4
Andrew Barch <enjia2000@gmail.com>
2018-11-25 10:23:14 -0500

    VERSION

--- include/autoconf.h
+++ include/autoconf.h
@@ -322,7 +322,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-//#define VERSION "1.71.0"
+#define VERSION "2.1.1 - titus expanded crafting"
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
